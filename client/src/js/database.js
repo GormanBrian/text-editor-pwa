14773a19a4ef1cd11e6f8db3690171b9861d64a2
Brian Gorman <briangorman99@gmail.com>
2023-08-28 18:22:09 -0400

    rename jate database to textEditor

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,14 +1,17 @@
 import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB("jate", 1, {
+  openDB("textEditor", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
+      if (db.objectStoreNames.contains("textEditor")) {
+        console.log("textEditor database already exists");
         return;
       }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
+      db.createObjectStore("textEditor", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+      console.log("textEditor database created");
     },
   });
 
