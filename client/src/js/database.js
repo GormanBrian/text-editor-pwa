0e5502f8da23817f3cef5ca3e9608b4221ea9e17
Brian Gorman <briangorman99@gmail.com>
2023-08-28 19:24:06 -0400

    add webpack plugins and loaders, update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,27 +1,27 @@
 import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB("textEditor", 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains("textEditor")) {
-        console.log("textEditor database already exists");
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("text database already exists");
         return;
       }
-      db.createObjectStore("textEditor", {
+      db.createObjectStore("jate", {
         keyPath: "id",
         autoIncrement: true,
       });
-      console.log("textEditor database created");
+      console.log("text database created");
     },
   });
 
 // Method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const textDb = await openDB("textEditor", 1);
-  const tx = textDb.transaction("textEditor", "readwrite");
-  const store = tx.objectStore("text");
+  const textDb = await openDB("jate", 1);
+  const tx = textDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
 
-  const request = store.add(content);
+  const request = store.put({ content });
   const result = await request;
 
   console.log("Successfully added content to the database\n", result);
@@ -29,9 +29,9 @@ export const putDb = async (content) => {
 
 // Method that gets all the content from the database
 export const getDb = async () => {
-  const textDb = await openDB("textEditor", 1);
-  const tx = textDb.transaction("textEditor", "readonly");
-  const store = tx.objectStore("text");
+  const textDb = await openDB("jate", 1);
+  const tx = textDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
 
   const request = store.getAll();
   const result = await request;
