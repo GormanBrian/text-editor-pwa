d21db7bcf20eca63bf93016ca8a3a4e826867c51
Brian Gorman <briangorman99@gmail.com>
2024-02-23 20:41:29 -0500

    update server node type to module, update server scripts

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-const express = require('express');
+import express, { static as eStatic, urlencoded, json } from "express";
+import htmlRoutes from "./routes/htmlRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(eStatic("../client/dist"));
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
-require('./routes/htmlRoutes')(app);
+htmlRoutes(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
